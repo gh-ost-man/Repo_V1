59e7b16fe1aa6f788e0f5cc47ae2c850e7165b58
Mark Brighton <marik.shevchenko@icloud.com>
2020-11-13 16:02:29 +0200

    commit3

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -13,6 +13,7 @@ namespace ConsoleApp1
             Console.WriteLine("Aloha");
             Console.WriteLine("Hello world");
             Console.WriteLine("wwddd");
+            Console.WriteLine("Hallo!");
         }
     }
 }
